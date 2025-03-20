024b13ef942b9af5214d3fa90a6d0ab3ce2ad52c
Faya <osvaldocarvalho.profissional@gmail.com>
2025-03-20 12:21:31 -0300

    [FEAT] Criado socket TCP com o endereço e porta indicados

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import sys
+import socket
 
 if len(sys.argv) != 4:
     print("Uso correto: python3 <nome>.py <endereço>:<porta> <vizinhos.txt> <diretório_compartilhado>")
     sys.exit(1)
 
 IP = sys.argv[1].split(":")[0]
-PORTA = sys.argv[1].split(":")[1]
+PORTA = int(sys.argv[1].split(":")[1])
 vizinhos = sys.argv[2]
 diretorio_compartilhado = sys.argv[3]
 
+#Criando o socket TCP conforme especificado em 2.3 EP: parte 1
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((IP, PORTA))
 
 
